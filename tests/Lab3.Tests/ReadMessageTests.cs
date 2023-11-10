27a9879cedbae2db1ffdb9faa61a2b13dccaa5df
Andrei Kostygov <kosandr17@mail.ru>
2023-11-10 21:25:28 +0300

    feat: add ContactBook

--- tests/Lab3.Tests/ReadMessageTests.cs
+++ tests/Lab3.Tests/ReadMessageTests.cs
@@ -1,4 +1,5 @@
-﻿using Itmo.ObjectOrientedProgramming.Lab3.Entities;
+﻿using System.Collections.Generic;
+using Itmo.ObjectOrientedProgramming.Lab3.Entities;
 using Itmo.ObjectOrientedProgramming.Lab3.Exceptions;
 using Xunit;
 
@@ -6,50 +7,72 @@ namespace Itmo.ObjectOrientedProgramming.Lab3.Tests;
 
 public class ReadMessageTests
 {
+    private User _userVova;
+    private ContactBook _contactBook;
+
+    public ReadMessageTests()
+    {
+        _userVova = new User("Vova");
+        _contactBook = new ContactBook(new List<Topic>()
+        {
+            new Topic("Vova", _userVova),
+            new Topic(
+                "Petya",
+                new User("Petya")),
+            new Topic(
+                "M3203",
+                new Group(
+                    "My group",
+                    new List<IAdressee>()
+                    {
+                        new User("Masha"),
+                        new User("Misha"),
+                        new User("Gosha"),
+                    })),
+        });
+    }
+
     [Fact]
     public void GetAndNotReadMessage()
     {
         // Arrange
-        var user = new User("Vova");
         var message = new Message("Song", "Skibidi dop", 6);
 
         // Act
-        user.SendMessage(message);
+        _contactBook.SendMessageByName("Vova", message);
 
         // Assert
-        Assert.False(user.SendMessageStatus(message));
+        Assert.False(_userVova.SendMessageStatus(message));
     }
 
     [Fact]
     public void MarkNotReadMessageAsRead()
     {
         // Arrange
-        var user = new User("Vova");
         var message = new Message("Song", "Skibidi dop", 6);
 
         // Act
-        user.SendMessage(message);
+        _contactBook.SendMessageByName("Vova", message);
 
         // Assert
-        Assert.False(user.SendMessageStatus(message));
-        user.MarkReadMessage(message);
-        Assert.True(user.SendMessageStatus(message));
+        Assert.False(_userVova.SendMessageStatus(message));
+        _userVova.MarkReadMessage(message);
+        Assert.True(_userVova.SendMessageStatus(message));
     }
 
     [Fact]
     public void MarkReadMessageAsRead()
     {
         // Arrange
-        var user = new User("Vova");
         var message = new Message("Song", "Skibidi dop", 6);
 
         // Act
-        user.SendMessage(message);
+        _contactBook.SendMessageByName("Vova", message);
 
         // Assert
-        Assert.False(user.SendMessageStatus(message));
-        user.MarkReadMessage(message);
-        Assert.True(user.SendMessageStatus(message));
-        Assert.Throws<JustReadException>(() => user.MarkReadMessage(message));
+        Assert.False(_userVova.SendMessageStatus(message));
+        _userVova.MarkReadMessage(message);
+        Assert.True(_userVova.SendMessageStatus(message));
+        Assert.Throws<JustReadException>(() => _userVova.MarkReadMessage(message));
     }
 }
